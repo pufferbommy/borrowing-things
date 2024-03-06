a1690da52b56d2e446cc70ddab9a6f8168d380fe
bom <panyawat.k@unigainfo.com>
2024-03-06 18:56:43 +0700

    update host

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import mysql from "mysql2/promise";
 
 const connectToDb = async () => {
   return mysql.createConnection({
-    host: "165.232.165.187",
+    host: "mysql",
     user: "root",
     password: "root",
     database: "borrowing_things",
